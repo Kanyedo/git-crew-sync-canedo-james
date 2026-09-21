56b848152dc4f828adcf3d6c77062587c2b8fdd4
Kanyedo <kanyedojames@gmail.com>
2026-09-21 19:10:25 +0800

    Add overtime pay for shifts over 8 hours - canedo.james

--- shifts.js
+++ shifts.js
@@ -3,7 +3,10 @@ function isValidShift(hours) {
 }
 
 function calculatePay(hours, rate) {
-  return Math.floor(hours * rate);
+  if (hours <= 8) {
+    return Math.floor(hours * rate);
+  }
+  const overtimeHours = hours - 8;
+  return Math.floor(8 * rate + overtimeHours * rate * 1.5);
 }
-
 module.exports = { isValidShift, calculatePay };
